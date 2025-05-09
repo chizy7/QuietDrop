45e802291637b03c85a551e4721f429cf4d38ee7
Chizy <chizy7@gmail.com>
2025-05-08 20:45:48 -0400

    Testing rule

--- quietdrop-core/src/authentication.rs
+++ quietdrop-core/src/authentication.rs
@@ -9,7 +9,7 @@ use argon2::{
 };
 use rand::rngs::OsRng;
 use std::fs::File;
-use std::io::{Read, Write}; // Testing branch rule
+use std::io::{Read, Write};
 
 const SALT_FILE: &str = "salt.txt";
 
